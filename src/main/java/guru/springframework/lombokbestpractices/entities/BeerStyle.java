2ec9b9da782896b24cc06e05583362cd65925eef
JuhMaran <julianemaran@gmail.com>
2026-04-01 15:22:54 -0300

    new project

---
+++ src/main/java/guru/springframework/lombokbestpractices/entities/BeerStyle.java
@@ -0,0 +1,25 @@
+package guru.springframework.lombokbestpractices.entities;
+
+/**
+ * lombok-best-practices
+ *
+ * @author Juliane Maran
+ * @since 01/04/2026
+ */
+public enum BeerStyle {
+
+  LAGER,
+  PILSNER,
+  STOUT,
+  GOSE,
+  PORTER,
+  ALE,
+  WHEAT,
+  IPA,
+  PALE_ALE,
+  SAISON;
+
+  private BeerStyle() {
+  }
+
+}
